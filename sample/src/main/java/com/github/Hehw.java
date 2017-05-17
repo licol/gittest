53e8c9c3c0036b31a0e491e59c4ffe99676dea4d
licol <690858904@qq.com>
2017-05-17 10:42:37 +0800

    gittest-hehw

---
+++ sample/src/main/java/com/github/Hehw.java
@@ -0,0 +1,11 @@
+package com.github;
+
+/**
+ * Created by licol on 2017/5/17.
+ */
+public class Hehw
+{
+    public static void main(String[] args) {
+        System.out.println("Hehehehe");
+    }
+}
